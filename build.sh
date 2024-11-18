571b77562bf7456f5d716894c1436395f14280cb
Valentin Prugnaud <valentin@whatdafox.com>
2024-11-17 23:07:57 -0800

    ci: attempt to automate build

--- build.sh
+++ build.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
 
+# Set default values if environment variables are not set
+PLATFORM=${PLATFORM:-$(uname -s | tr '[:upper:]' '[:lower:]')}
+ARCH=${ARCH:-$(uname -m)}
+
+# Map common architecture names
+case "$ARCH" in
+    x86_64|amd64) ARCH="x86_64" ;;
+    aarch64|arm64) ARCH="aarch64" ;;
+esac
+
+echo "Building for platform: $PLATFORM, architecture: $ARCH"
+
 echo 'Building extract-audio' && \
-(cd src/extract-audio && deno task build) && \
+(cd src/extract-audio && DENO_PLATFORM=$PLATFORM DENO_ARCH=$ARCH deno task build) && \
 echo 'Done'
 
 echo 'Building transcribe-audio' && \
-(cd src/transcribe-audio && deno task build) && \
+(cd src/transcribe-audio && DENO_PLATFORM=$PLATFORM DENO_ARCH=$ARCH deno task build) && \
 echo 'Done'
 
 echo 'Building translate-srt' && \
-(cd src/translate-srt && deno task build) && \
+(cd src/translate-srt && DENO_PLATFORM=$PLATFORM DENO_ARCH=$ARCH deno task build) && \
 echo 'Done'
+
